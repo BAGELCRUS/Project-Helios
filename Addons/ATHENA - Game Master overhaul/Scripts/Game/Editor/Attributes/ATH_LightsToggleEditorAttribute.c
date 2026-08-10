f9fb76d819d281e3011897dcd809a072af602647
BAGELCRUS <averympearrow@gmail.com>
2026-08-10 16:08:38 -0500

    fix(athena): resolve missing UI slider data and empty dropdown lists
    Vehicle health sliders were reporting 'MISSING m_SliderData' because the script classes inherited from the generic SCR_BaseEditorAttribute instead of SCR_BaseValueListEditorAttribute. Dynamically injected SCR_EditorAttributeBaseValues into GetEntries to guarantee 0-1 sliders render without needing .conf adjustments. Upgraded Lights attribute to SCR_BasePresetsEditorAttribute to properly feed the dropdown list.

--- Addons/ATHENA - Game Master overhaul/Scripts/Game/Editor/Attributes/ATH_LightsToggleEditorAttribute.c
+++ Addons/ATHENA - Game Master overhaul/Scripts/Game/Editor/Attributes/ATH_LightsToggleEditorAttribute.c
@@ -1,6 +1,40 @@
 [BaseContainerProps(), SCR_BaseContainerCustomTitleUIInfo("m_UIInfo")]
-class ATH_LightsToggleEditorAttribute : SCR_BaseEditorAttribute
+class ATH_LightsToggleEditorAttribute : SCR_BasePresetsEditorAttribute
 {
+	override int GetEntries(notnull array<ref SCR_BaseEditorAttributeEntry> outEntries)
+	{
+		array<ref SCR_EditorAttributeFloatStringValueHolder> aValues = {};
+		
+		SCR_EditorAttributeFloatStringValueHolder val0 = new SCR_EditorAttributeFloatStringValueHolder();
+		val0.SetName("Auto");
+		val0.SetFloatValue(0);
+		aValues.Insert(val0);
+		
+		SCR_EditorAttributeFloatStringValueHolder val1 = new SCR_EditorAttributeFloatStringValueHolder();
+		val1.SetName("Off");
+		val1.SetFloatValue(1);
+		aValues.Insert(val1);
+		
+		SCR_EditorAttributeFloatStringValueHolder val2 = new SCR_EditorAttributeFloatStringValueHolder();
+		val2.SetName("Headlights");
+		val2.SetFloatValue(2);
+		aValues.Insert(val2);
+		
+		SCR_EditorAttributeFloatStringValueHolder val3 = new SCR_EditorAttributeFloatStringValueHolder();
+		val3.SetName("HiBeams");
+		val3.SetFloatValue(3);
+		aValues.Insert(val3);
+
+		SCR_EditorAttributeFloatStringValueHolder val4 = new SCR_EditorAttributeFloatStringValueHolder();
+		val4.SetName("Hazards");
+		val4.SetFloatValue(4);
+		aValues.Insert(val4);
+
+		outEntries.Insert(new SCR_EditorAttributePresetEntry(1, false, "", false, false, "", -1));
+		outEntries.Insert(new SCR_BaseEditorAttributeFloatStringValues(aValues));
+		return outEntries.Count();
+	}
+	
 	override SCR_BaseEditorAttributeVar ReadVariable(Managed item, SCR_AttributesManagerEditorComponent manager)
 	{
 		SCR_EditableEntityComponent editableEntity = SCR_EditableEntityComponent.Cast(item);
